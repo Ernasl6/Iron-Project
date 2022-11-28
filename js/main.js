9ae628ce68e56dd4da56da553057e76e9b1056c0
Ernas <omurkulovernas16@gmail.com>
2022-11-28 11:51:33 +0600

    parallel

--- js/main.js
+++ js/main.js
@@ -11,3 +11,93 @@ playBtn.addEventListener('click', function() {
         this.classList.remove('playing')
     }
 })
+
+
+const playBtn2 = document.getElementById('btn2')
+
+const video2 = document.getElementById('video-2')
+
+playBtn2.addEventListener('click', function() {
+    if(video2.paused) {
+        video2.play()
+        this.classList.add('playing')
+    } else {
+        video2.pause()
+        this.classList.remove('playing')
+    }
+})
+
+
+const playBtn3 = document.getElementById('btn3')
+
+const video3 = document.getElementById('video-3')
+
+playBtn3.addEventListener('click', function() {
+    if(video3.paused) {
+        video3.play()
+        this.classList.add('playing')
+    } else {
+        video3.pause()
+        this.classList.remove('playing')
+    }
+})
+
+
+const playBtn4 = document.getElementById('btn4')
+
+const video4 = document.getElementById('video-4')
+
+playBtn4.addEventListener('click', function() {
+    if(video4.paused) {
+        video4.play()
+        this.classList.add('playing')
+    } else {
+        video4.pause()
+        this.classList.remove('playing')
+    }
+})
+
+
+const playBtn5 = document.getElementById('btn5')
+
+const video5 = document.getElementById('video-5')
+
+playBtn5.addEventListener('click', function() {
+    if(video5.paused) {
+        video5.play()
+        this.classList.add('playing')
+    } else {
+        video5.pause()
+        this.classList.remove('playing')
+    }
+})
+
+
+const playBtn6 = document.getElementById('btn6')
+
+const video6 = document.getElementById('video-6')
+
+playBtn6.addEventListener('click', function() {
+    if(video6.paused) {
+        video6.play()
+        this.classList.add('playing')
+    } else {
+        video6.pause()
+        this.classList.remove('playing')
+    }
+})
+
+
+const playBtn7 = document.getElementById('btn7')
+
+const video7 = document.getElementById('video-7')
+
+playBtn7.addEventListener('click', function() {
+    if(video7.paused) {
+        video7.play()
+        this.classList.add('playing')
+    } else {
+        video7.pause()
+        this.classList.remove('playing')
+    }
+})
